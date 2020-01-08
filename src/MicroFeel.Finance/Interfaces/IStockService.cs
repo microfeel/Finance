ddbd35a92206070e7afbdb5296851fdb00244b0a
iml6yu <iml6yu@gmail.com>
2020-01-08 15:33:47 +0800

    增加仓储接口约束

--- src/MicroFeel.Finance/Interfaces/IStockService.cs
+++ src/MicroFeel.Finance/Interfaces/IStockService.cs
@@ -8,7 +8,53 @@ namespace MicroFeel.Finance.Interfaces
     /// </summary>
     public interface IStockService : IDisposable
     {
-        bool AddStock(Stock stock);  
-       
+        /// <summary>
+        /// 成品入库
+        /// </summary>
+        /// <param name="productin"></param>
+        /// <returns></returns>
+        bool ProductIn(Productin productin);
+
+        /// <summary>
+        /// 其他入库
+        /// </summary>
+        /// <param name="otherin"></param>
+        /// <returns></returns>
+        bool OtherIn(Otherin otherin);
+
+        /// <summary>
+        /// 其他出库
+        /// </summary>
+        /// <param name="otherout"></param>
+        /// <returns></returns>
+        bool OtherOut(Otherout otherout);
+
+        /// <summary>
+        /// 采购入库
+        /// </summary>
+        /// <param name="purchasein"></param>
+        /// <returns></returns>
+        bool PurchaseIn(Purchasein purchasein);
+
+        /// <summary>
+        /// 材料出库
+        /// </summary>
+        /// <param name="materialout"></param>
+        /// <returns></returns>
+        bool Materialout(Materialout materialout);
+
+        /// <summary>
+        /// 调拨单
+        /// </summary>
+        /// <param name="transvouch"></param>
+        /// <returns></returns>
+        bool Transvouch(Transvouch transvouch);
+
+        /// <summary>
+        /// 销售出库
+        /// </summary>
+        /// <param name="saleout"></param>
+        /// <returns></returns>
+        bool Saleout(Saleout saleout);
     }
 }
