b132a319798fb6f029d782ed61dd6c74817ebf1a
门欣 <menxin@qq.com>
2020-07-27 22:20:33 +0800

    添加获取委外订单原材料领用数据接口

--- src/MicroFeel.Finance/Models/DbDto/OmMaterialRecord.cs
+++ src/MicroFeel.Finance/Models/DbDto/OmMaterialRecord.cs
@@ -6,7 +6,30 @@
 
 namespace MicroFeel.Finance
 {
+    /// <summary>
+    /// 委外订单领料记录
+    /// </summary>
     public class OmMaterialRecord
     {
+        /// <summary>
+        /// 订单明细ID
+        /// </summary>
+        public int MoDetailId { get; set; }
+        /// <summary>
+        /// 应领数量
+        /// </summary>
+        public decimal? IQuantity { get; set; }
+        /// <summary>
+        /// 单位数量
+        /// </summary>
+        public decimal? IUnitQuantity { get; set; }
+        /// <summary>
+        /// 已领数量
+        /// </summary>
+        public decimal? ISendQty { get; set; }
+        /// <summary>
+        /// 材料编码
+        /// </summary>
+        public string CInvCode { get; set; }
     }
 }
